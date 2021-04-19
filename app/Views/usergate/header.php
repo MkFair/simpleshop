27407ddd20b12d4052c008a8fd59c15f5572cf23
skellik-1 <82837093+skellik-1@users.noreply.github.com>
2021-04-19 22:45:26 +0300

    наброски для логина

---
+++ app/Views/usergate/header.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" type="text/css" href="/static/css/bootstrap.min.css">
+
+<button class="btn btn-primary">
+	cdcd
+</button>
--- app/Views/usergate/login.php
+++ app/Views/usergate/login.php
@@ -1,1 +1,13 @@
-....
+<div class="container">
+	<div class="row">
+<div class="col-lg-4 m-auto">
+<div class="card ">
+	<div class="card-body">
+		uhuihuh
+		<button class="btn btn-primary">
+	cdcd
+</button>
+	</div>
+	
+</div></div>
+</div></div>

5231f1bbf90c8783f341d87a2f38065507da1621
skellik-1 <82837093+skellik-1@users.noreply.github.com>
2021-04-21 19:04:57 +0300

    html версия регистрации(без ajax)

--- app/Views/usergate/header.php
+++ app/Views/usergate/header.php
@@ -1,5 +1,2 @@
 <link rel="stylesheet" type="text/css" href="/static/css/bootstrap.min.css">
 
-<button class="btn btn-primary">
-	cdcd
-</button>
--- app/Views/usergate/login.php
+++ app/Views/usergate/login.php
@@ -1,13 +1,56 @@
 <div class="container">
 	<div class="row">
-<div class="col-lg-4 m-auto">
-<div class="card ">
-	<div class="card-body">
-		uhuihuh
-		<button class="btn btn-primary">
-	cdcd
-</button>
+		<div class="col-lg-5 m-auto">
+			<div class="card ">
+				<div class="card-body">
+					<center><b>Регистрация </b></center>
+					<br>
+
+					<ul class="nav nav-tabs">
+						  <li class="nav-item">
+						    <a class="nav-link active" data-toggle="tab" href="#seller" >Продавец</a>
+						  </li>
+						  <li class="nav-item">
+						    <a class="nav-link" data-toggle="tab" href="#client">Покупатель</a>
+						  </li>
+					</ul>
+					<div class="tab-content">
+					  <div class="tab-pane fade show active" id="seller">
+					    <form>
+					    	<label>ФИО</label>
+					    	<input type="text" class="form-control" name=""><br>
+					    	<label>Email</label>
+							<input type="email" class="form-control" name="" placeholder="name@example.com"><br>
+							<label>Пароль</label>
+							<input type="password" class="form-control" name=""><br>
+							<label>Адрес сайта</label>
+							<input type="text" class="form-control" name=""><br>
+							<label>Описание сайта</label>
+							<input type="text" class="form-control" name=""><br>
+							<label>Деятельность</label>
+							<input type="text" class="form-control" name=""><br>
+					    </form>
+					  </div>
+					  <div class="tab-pane fade" id="client">
+					   	<form>
+					    	<label>ФИО</label>
+					    	<input type="text" class="form-control" name=""><br>
+					    	<label>Email</label>
+							<input type="email" class="form-control" name="" placeholder="name@example.com"><br>
+							<label>Пароль</label>
+							<input type="password" class="form-control" name=""><br>
+					    </form>
+					  </div>
+					</div>
+
+					<button class="btn btn-primary ">
+				Зарегистрироваться
+				<br>
+				
+					</button>
+				</div>
+				
+			</div>
+		</div>
 	</div>
-	
-</div></div>
-</div></div>
+</div>

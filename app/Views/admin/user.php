d1ff30145428bb712bccac239731e9dd6de3229b
skellik-1 <82837093+skellik-1@users.noreply.github.com>
2021-04-28 16:14:41 +0300

    user и show site

--- app/Views/admin/user.php
+++ app/Views/admin/user.php
@@ -1,1 +1,52 @@
-<?php var_dump($user); ?>
+<?php //var_dump($user); ?>
+<br>
+ <div class="row"> 
+    <div class="col-lg-6">
+	<h5 class="card-title">Личные данные покупателя</h5>
+
+
+<div class="form-group">
+        <label>ФИО</label>
+        <input type="text" class="form-control" name="fio" disabled required value="<?=$user->fio?>"> 
+  </div>
+<div class="form-group">
+        <label>Email</label>
+        <input type="email" class="form-control" name="email" required disabled placeholder="name@example.com" value="<?=$user->email?>">
+  </div>
+	</div>
+    
+
+<div class="col-lg-6">
+	<h5 class="card-title">Последние покупки</h5>
+    <table class="table table-hover table-sm">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Компания</th>
+      <th scope="col">Кол. купонов</th>
+      <th scope="col">Дата</th>
+    </tr>
+  </thead>
+  <tbody>
+
+<?php 
+for ($i=1; $i<=20 ; $i++) { 
+?>
+    <tr>
+      <th scope="row"> <?=$i; ?> </th>
+      <td>Fanserials</td>
+      <td class="text-center">15</td>
+      <td>25.04.2021</td>
+    </tr>
+
+<?php
+}
+?>
+
+
+  </tbody>
+</table>
+</div>
+  
+    </div>
+
--- app/Views/show_site.php
+++ app/Views/show_site.php
@@ -1,10 +1,63 @@
-<?=$site->url?>
-<?=$site->description?>
+
+
+<div class="row mt-3">
+      <div class="col-lg-6 mb-3">
+        <div class="card">
+	          <div class="card-body">
+	          	<a href="<?=$site->url?>"><h2><?=$site->url?> </h2></a>
+	          	<br>
+	          	<?=$site->description?>
+			</div>
+		</div>
+</div>
+<div class="col-lg-6 mb-3">
+        <div class="card">
+	          <div class="card-body">
+	          	<h4>Услуги</h4>
+	          	 <table class="table table-hover table-sm">
+  <thead>
+    <tr>
+     <th scope="col">Наименование</th>
+     <th scope="col">Цена в купонах</th>
+    </tr>
+  </thead>
+  <tbody>
+
+<?php 
+for ($i=1; $i<=5 ; $i++) { 
+?>
+    <tr>
+      <td>Мойка машины</td>
+      <td>10</td>
+    </tr>
+
+<?php
+}
+?>
+  </tbody>
+</table>
+
 
 <form method=post action="/index.php/buy">
-    <input type=number step=1 min=1 value=1 name="count">
+
+	<div class="form-group">
+	<label><b>Купить купоны на услуги магазина</b></label>
+    <input class="form-control" type=number step=1 min=1 value=1 name="count" width="10">
     <input type=hidden name="site_id" value="<?=$site->id?>">
+	</div>
     <button class="btn btn-primary">
         Купить
     </button>
+
 </form>
+
+
+
+	</div>
+	</div>
+</div>
+</div>
+
+
+
+
